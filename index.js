c041acdf376c8be547cd0a2024557f970fe49c92
bhavinjain21 <139436743+bhavinjain21@users.noreply.github.com>
2023-07-18 03:15:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var a = $('h1').html();
-$("h1").html('');
-for(var i = 0;i<a.length;i++){
-  var b = a.substr(i,1) ;
-  //console.log(b);
-  var c = "<h1 class ='box'>"+b+" </h1>";
-  // console.log(c);
-  $("h1").append(c);
-}
+$("h1").addClass("box");
