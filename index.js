e8f786b50f2753c7f0d205330082d294ffdaf308
bhavinjain21 <139436743+bhavinjain21@users.noreply.github.com>
2023-07-18 19:33:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 $("h1").addClass("box");
-window.onload=function () {
-var a = new Audio("bgn.mp3");
-a.play();}
+window.addEventListener("DOMContentLoaded", event => {
+  const audio = new Audio("bgn.mp3");
+  audio.volume = 0.7;
+  audio.play();
+});
 
 $(".start").click(function (){
   var b = new Audio("start.mp3");
